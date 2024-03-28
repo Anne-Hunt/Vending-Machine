ca5570a953dca12b17466a04f9d809d0608af34b
Anne Hunt <acathunt@gmail.com>
2024-03-27 18:21:59 -0600

    Images fixed

--- app/AppState.js
+++ app/AppState.js
@@ -11,84 +11,84 @@ class ObservableAppState extends EventEmitter {
     new Snacks({
       name: 'Jorts',
       price: 1.99,
-      imgURL: "https://em-content.zobj.net/source/google/387/popcorn_1f37f.png",
+      imgURL: "./assets/img/jorts.png",
       id: 'A1',
       qty: 4
     }),
     new Snacks({
       name: 'Cookie Cat',
       price: 2.99,
-      imgURL: "https://em-content.zobj.net/source/google/387/popcorn_1f37f.png",
+      imgURL: "./assets/img/cookie-cat.png",
       id: 'A2',
       qty: 4
     }),
     new Snacks({
       name: 'Jelly Tart',
       price: .99,
-      imgURL: "https://em-content.zobj.net/source/google/387/popcorn_1f37f.png",
+      imgURL: "./assets/img/Jelly-Tarts.png",
       id: 'A3',
       qty: 4
     }),
     new Snacks({
       name: 'Scooby Snack',
       price: .99,
-      imgURL: "https://em-content.zobj.net/source/google/387/popcorn_1f37f.png",
+      imgURL: "./assets/img/Scooby-Snacks.png",
       id: 'A4',
       qty: 4
     }),
     new Snacks({
       name: 'Pink Donut',
       price: 1.99,
-      imgURL: "https://em-content.zobj.net/source/google/387/popcorn_1f37f.png",
+      imgURL: "./assets/img/pink-donut.png",
       id: 'B1',
       qty: 4
     }),
     new Snacks({
       name: 'Krabby Patty',
       price: 4.99,
-      imgURL: "https://em-content.zobj.net/source/google/387/popcorn_1f37f.png",
+      imgURL: "./assets/img/krabby-patty.png",
       id: 'B2',
       qty: 4
     }),
     new Snacks({
       name: 'Carrot',
       price: .19,
-      imgURL: "https://em-content.zobj.net/source/google/387/popcorn_1f37f.png",
+      imgURL: "./assets/img/carrots.png",
       id: 'B3',
       qty: 4
     }),
     new Snacks({
       name: 'Picnic Basket',
       price: 24.99,
-      imgURL: "https://em-content.zobj.net/source/google/387/popcorn_1f37f.png",
+      imgURL: "./assets/img/basket.png",
       id: 'B4',
       qty: 4
     }),
     new Snacks({
       name: 'Boba',
       price: 5.99,
-      imgURL: "https://em-content.zobj.net/source/google/387/popcorn_1f37f.png",
+      imgURL: "./assets/img/Bear-Boba.png",
       id: 'C1',
       qty: 4
     }),
     new Snacks({
       name: 'Smurfberries',
       price: 2.99,
-      imgURL: "https://em-content.zobj.net/source/google/387/popcorn_1f37f.png",
+      imgURL: "./assets/img/Smurfberry_Bush.png",
       id: 'C2',
       qty: 4
     }),
     new Snacks({
       name: 'Mouse Cheese',
       price: 1.99,
-      imgURL: "https://em-content.zobj.net/source/google/387/popcorn_1f37f.png",
+      imgURL: "./assets/img/cheese.png",
       id: 'C3',
       qty: 4
     }),
     new Snacks({
       name: 'Cookie',
       price: .99,
-      imgURL: "https://em-content.zobj.net/source/google/387/popcorn_1f37f.png",
+      imgURL: "./assets/img/cookie.png",
       id: 'C4',
       qty: 4
     })
--- app/models/Snacks.js
+++ app/models/Snacks.js
@@ -14,28 +14,26 @@ export class Snacks {
 
   get SnacksCard() {
     return `
-  <div class="col-3">
-    <div class="card text-center">
-      <img src="${this.imgURL}" alt="snacks" class="card-image">
-      <h4>${this.name}</h4>
+  <div class="col-3 d-flex justify-content-center">
+    <div class="card text-center bg-dark">
+      <img src="${this.imgURL}" alt="snacks" class="card-image snackimage py-1 mb-1 ">
+      <span class="bg-white rounded shadow"><h5 class="m-0">${this.name}</h5>
       <span>${this.price}</span>
-      <span class="bcode">${this.id}</span>
+      <span>${this.id}</span></span>
     </div>
   </div>`
   }
 
   get SnacksButton() {
-    return `
-    <div class="col-1">
-    <button class="btn btn-secondary rounded-circle" onclick="app.SnacksController.buySnack('${this.id}')">${this.id}</button>
-  </div>`
+    return `<div class="p-2 w-50 d-flex justify-content-center">
+      <button class="btn btn-secondary rounded-circle col-2 p-2 g-1 snackbutton text-center border border-dark border-1 shadow" onclick="app.SnacksController.buySnack('${this.id}')">${this.id}</button></div>`
   }
 
   static get SnacksGone() {
     return `
     <div class="col-3">
   <div class="card text-center">
-    <img src="https://em-content.zobj.net/source/google/387/cross-mark_274c.png" alt="snacks" class="card-image">
+    <img src="https://em-content.zobj.net/source/google/387/cross-mark_274c.png" alt="snacks" class="card-image snackimage">
     <h4>Out of Stock</h4>
     <p>contact vendor</p>
     <p class="bcode">out of stock</p>
